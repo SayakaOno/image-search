d8a38e1574644e9790f78a34d86b9da08bd8014c
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-02 14:56:53 -0800

    add calculation for navbar width

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,6 @@ class App extends Component {
         //TODO: gl:
       }
     });
-
     let data = response.data.items;
     if (start === 1) {
       const imageList = [];
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
+  state = { navbarWidth: 0, itemCount: 0 };
+
+  componentDidMount() {
+    this.setState({ itemCount: this.props.items.length });
+  }
+
+  componentDidUpdate() {
+    if (this.state.itemCount !== this.props.items.length) {
+      this.setState(prevState => {
+        const listItems = document.querySelectorAll(".navbar li");
+        const listItemArray = Array.from(listItems);
+        const navbarWidth = listItemArray.reduce(
+          (sum, current) => sum + current.offsetWidth,
+          0
+        );
+        return { navbarWidth, itemCount: prevState.itemCount + 1 };
+      });
+    }
+  }
+
+  getNavbarWidth() {}
+
   renderItems = () => {
     return React.Children.map(this.props.items, (item, index) => {
       let active = index === this.props.selectedIndex ? " active" : "";
@@ -17,7 +39,13 @@ class NavBar extends Component {
     return this.props.items.length === 0 ? null : (
       <div className="navbar">
         <div className="navbar-container">
-          <ul className="ui top tabular menu" onClick={this.onNavClick}>
+          <ul
+            className="ui top tabular menu"
+            onClick={this.onNavClick}
+            style={{
+              width: this.state.navbarWidth + 20
+            }}
+          >
             {this.renderItems()}
           </ul>
         </div>
