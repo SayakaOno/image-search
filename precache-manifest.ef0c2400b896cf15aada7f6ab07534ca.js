5c591c3889b795ccc553f5d08084578fe877399b
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-01-03 23:00:41 -0800

    Updates

---
+++ precache-manifest.ef0c2400b896cf15aada7f6ab07534ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fc34d65b51faa3ceacd1cc241dd822c",
+    "url": "/image-search/static/media/redvelvetcake.2fc34d65.jpg"
+  },
+  {
+    "revision": "395df99ba3f6380671f555668681763c",
+    "url": "/image-search/static/media/lemoncake.395df99b.jpg"
+  },
+  {
+    "revision": "7b396a0664ac15ed43f83e110855b010",
+    "url": "/image-search/static/media/chocolatecake.7b396a06.jpg"
+  },
+  {
+    "revision": "48a5733515ee77022db2bf02bef3a32d",
+    "url": "/image-search/static/media/carrotcake.48a57335.jpg"
+  },
+  {
+    "revision": "430cf7d360eea82a327d",
+    "url": "/image-search/static/js/runtime~main.430cf7d3.js"
+  },
+  {
+    "revision": "38e208acfbbda9783d91",
+    "url": "/image-search/static/js/main.38e208ac.chunk.js"
+  },
+  {
+    "revision": "f6561e681cff1c721f9e",
+    "url": "/image-search/static/js/1.f6561e68.chunk.js"
+  },
+  {
+    "revision": "38e208acfbbda9783d91",
+    "url": "/image-search/static/css/main.0662ef3d.chunk.css"
+  },
+  {
+    "revision": "de56883d7ae04cecb28f6173bfbf7533",
+    "url": "/image-search/index.html"
+  }
+];
